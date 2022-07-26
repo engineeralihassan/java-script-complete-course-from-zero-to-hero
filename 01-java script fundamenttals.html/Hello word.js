12376441cb34ac34e8108519e9b21e0bb921be54
Ali Hassan <engineralihassan@gmail.com>
2022-07-25 18:52:58 -0700

    Add files via upload

---
+++ 01-java script fundamenttals.html/Hello word.js
@@ -0,0 +1,8 @@
+console.log("Hello java script");
+
+alert("File is attach successfully");
+console.log("thanks jonas this is amzing course");
+let js = "fun";
+if (js === "fun") {
+  alert("Java script is fun");
+}
